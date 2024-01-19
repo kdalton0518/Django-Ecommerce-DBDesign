89e1b4e19df1f927de5bf4aab1158548f2cf2f2d
kdalton0518 <kdalton0518@gmail.com>
2024-01-19 08:41:17 +0530

    Stock / Media - Fixture / Tests / Models Added - Requirements Updated

--- ecommerce/apps/demo/management/commands/load_fixtures.py
+++ ecommerce/apps/demo/management/commands/load_fixtures.py
@@ -25,3 +25,5 @@ class Command(BaseCommand):
         call_command("loaddata", "db_type_fixture.json")
         call_command("loaddata", "db_brand_fixture.json")
         call_command("loaddata", "db_product_inventory_fixture.json")
+        call_command("loaddata", "db_media_fixture.json")
+        call_command("loaddata", "db_stock_fixture.json")
--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -20,3 +20,5 @@ def db_fixture_setup(django_db_setup, django_db_blocker):
         call_command("loaddata", "db_type_fixture.json")
         call_command("loaddata", "db_brand_fixture.json")
         call_command("loaddata", "db_product_inventory_fixture.json")
+        call_command("loaddata", "db_media_fixture.json")
+        call_command("loaddata", "db_stock_fixture.json")
