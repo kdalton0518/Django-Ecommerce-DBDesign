cc7246c640cafc5aa3e31c5327d2772e3bc6f331
kdalton0518 <kdalton0518@gmail.com>
2024-02-10 09:46:27 +0530

    Elastic Search - All Documents Updated

--- ecommerce/apps/inventory/documents.py
+++ ecommerce/apps/inventory/documents.py
@@ -15,7 +15,6 @@ class CategoryDocument(Document):
     class Django:
         model = Category
         fields = [
-            "id",
             "name",
             "slug",
             "is_active",
@@ -36,7 +35,6 @@ class ProductDocument(Document):
     class Django:
         model = Product
         fields = [
-            "id",
             "web_id",
             "name",
             "slug",
@@ -59,7 +57,6 @@ class ProductTypeDocument(Document):
     class Django:
         model = ProductType
         fields = [
-            "id",
             "name",
         ]
 
@@ -73,7 +70,6 @@ class BrandDocument(Document):
     class Django:
         model = Brand
         fields = [
-            "id",
             "name",
         ]
 
@@ -87,7 +83,113 @@ class ProductAttributeDocument(Document):
     class Django:
         model = ProductAttribute
         fields = [
-            "id",
             "name",
             "description",
         ]
+
+
+@registry.register_document
+class ProductAttributeValue(Document):
+    product_attribute = fields.KeywordField()
+
+    class Index:
+        name = "product_attribute_values"
+        settings = {"number_of_shards": 1, "number_of_replicas": 0}
+
+    class Django:
+        model = ProductAttributeValue
+        fields = [
+            "attribute_value",
+        ]
+
+    def prepare_product_attribute(self, instance):
+        return (
+            str(instance.product_attribute.id) if instance.product_attribute else None
+        )
+
+
+@registry.register_document
+class ProductInventoryDocument(Document):
+    product_type = fields.KeywordField()
+    product = fields.KeywordField()
+    brand = fields.KeywordField()
+    attribute_values = fields.ListField(fields.KeywordField())
+
+    class Index:
+        name = "product_inventories"
+        settings = {"number_of_shards": 1, "number_of_replicas": 0}
+
+    class Django:
+        model = ProductInventory
+        fields = [
+            "sku",
+            "upc",
+            "is_active",
+            "retail_price",
+            "store_price",
+            "sale_price",
+            "weight",
+            "created_at",
+            "updated_at",
+        ]
+
+    def prepare_product_type(self, instance):
+        return str(instance.product_type.id) if instance.product_type else None
+
+    def prepare_product(self, instance):
+        return str(instance.product.id) if instance.product else None
+
+    def prepare_brand(self, instance):
+        return str(instance.brand.id) if instance.brand else None
+
+    def prepare_attribute_values(self, instance):
+        return [str(value.id) for value in instance.attribute_values.all()]
+
+
+@registry.register_document
+class MediaDocument(Document):
+    product_inventory = fields.KeywordField()
+    image = fields.FileField()
+
+    class Index:
+        name = "medias"
+        settings = {"number_of_shards": 1, "number_of_replicas": 0}
+
+    class Django:
+        model = Media
+        fields = [
+            "alt_text",
+            "is_feature",
+            "created_at",
+            "updated_at",
+        ]
+
+    def prepare_product_inventory(self, instance):
+        return (
+            str(instance.product_inventory.id) if instance.product_inventory else None
+        )
+
+    def prepare_image(self, instance):
+        return instance.image.url if instance.image else None
+
+
+@registry.register_document
+class StockDocument(Document):
+    product_inventory = fields.KeywordField()
+
+    class Index:
+        name = "stocks"
+        settings = {"number_of_shards": 1, "number_of_replicas": 0}
+
+    class Django:
+        model = Stock
+        fields = [
+            "last_checked",
+            "units",
+            "units_sold",
+        ]
+
+    def prepare_product_inventory(self, instance):
+        return (
+            str(instance.product_inventory.id) if instance.product_inventory else None
+        )
